71f5832b54dce5b5b5157d37d1da781dde76b6e0
mohamat adil sentosa <81151453+adilsentosa@users.noreply.github.com>
2024-02-18 05:01:49 -0800

    [ADD] indexing models into django models
    add models into admin,py for indexing on django admin

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,13 @@ from django.contrib import admin
 from core.models import*
 # Register your models here.
 admin.site.register(Guru)
+admin.site.register(Jadwal)
+admin.site.register(JadwalKhusus)
+admin.site.register(Jurusan)
+admin.site.register(Kelas)
+admin.site.register(Mapel)
+admin.site.register(Penjadwalan)
+admin.site.register(RequestJadwal)
+admin.site.register(Rumusan)
+admin.site.register(TugasGuru)
+admin.site.register(User)
